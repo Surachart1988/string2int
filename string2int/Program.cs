eadc9283ef0d3affc779bb53f050824ae8936849
Surachart Tamluang <surachart1988.ta@gmail.com>
2021-01-06 13:20:00 +0700

    commit string2int Challenge

---
+++ string2int/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string2int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> collection = new string[] { "abc573", "a5b7c3", "abc 573", "-abc573", "ABC573", "aah72gv82g7gv7828cedc" };
+
+            foreach (var input in collection)
+            {
+                int number = 0;
+                foreach (char c in input)
+                {
+                    if (c >= '0' && c <= '9') // accept only numeric characters
+                    {
+                        number *= 10;
+                        number += c - '0'; // convert ascii to digit 0..9 and add it to number 
+                    }
+                }
+                Console.WriteLine(number);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
